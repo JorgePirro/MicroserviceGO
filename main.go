a9ac1e3d3077b2ab2af4f4e785b2f950d4cd3a1f
George Pirro <jfalcigno@gmail.com>
2020-05-20 18:27:44 -0300

    This is the basics of HandleFunc, and a very basic microservice with just 28 lines of code.

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World.")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		//log.Printf("Data %s\n", d)
+		if err != nil {
+			http.Error(rw, "Error", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe("127.0.0.1:9090", nil)
+
 }
